185970a1d45cc92deed6015e3cfedbd56447f1d5
Bradley Parker <Bradleyscottparker@outlook.com>
2023-08-07 14:39:08 -0500

    added quantity update to menu card

--- src/Components/MenuCard/MenuCard.js
+++ src/Components/MenuCard/MenuCard.js
@@ -1,19 +1,68 @@
-
+import minus from './minus.png'
+import plus from './plus.png'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const MenuCard = (props) =>{
+    const navigate = useNavigate()
+    const [quantity, setQuantity] = useState(1);
+    const [inCart, setInCart] = useState(true)
+    const user = useSelector((state)=>state.users)
 
+    const handleQuantityChange = (change) => {
+
+        if(change === 'decrease'  && quantity > 0){
+            setQuantity(quantity-1)
+        }
+        if(change === 'increase'  && quantity < 10){
+            setQuantity(quantity+1)
+        }
+        if(change === 'remove'){
+            setQuantity(0)
+        }
+        if(quantity === 0){
+            setInCart(false)
+            
+        }
+
+    }
 
     const showViewButton = () =>{
+        
         return(
-            
             <div id='view-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
-                <p style={{fontSize:'14px', lineHeight:'14px'}} className="mt-[7px]  font-sergioTrendy text-white">View</p>
-            </div>
+                <p onClick={()=>{navigate('/pizza-builder')}} style={{fontSize:'14px', lineHeight:'14px'}} className="mt-[7px]  font-sergioTrendy text-white">View</p>
+            </div>                
         )
+  
+        
+
     }
     const showAddQuantity = () =>{
 
-        return
+
+        if(!inCart){
+            return(
+            <div onClick={''} id='view-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
+                <p style={{fontSize:'14px', lineHeight:'14px'}} className="mt-[7px]  font-sergioTrendy text-white">Add</p>
+            </div>                
+            )
+        }else{
+            return(
+                <div id='quantity-container' className='flex h-[35px] w-[67px] border-black border-[1px] border-dashed'>
+                        <div id='minus-button'>
+                            <img src={minus} className='h-[15px] w-[15px]'/>
+                        </div>
+                        <div onClick={()=>{handleQuantityChange('decrease')}} id ='quantity-number'>
+                            {quantity}
+                        </div>
+                        <div onClick={()=>{handleQuantityChange('increase')}} id='plus-button'>
+                            <img src={plus} className='h-[15px] w-[15px]'/>
+                        </div>
+                </div>
+            )
+        }
     }
     
     return(
@@ -39,7 +88,7 @@ const MenuCard = (props) =>{
                                     <p style={{fontSize:'24px', lineHeight:'24px'}} className="mr-[4px]">$</p>
                                 </div>
                                 <div id='price'>
-                                    <p style={{fontSize:'24px', lineHeight:'24px'}} className="font-sergioTrendy">12</p>
+                                    <p style={{fontSize:'24px', lineHeight:'24px'}} className="font-sergioTrendy">7.50</p>
                                 </div>
                                 <div id='asterisk'>
                                     <p style={{fontSize:'24px', lineHeight:'24px', fontFamily:'Arial'}} className="ml-[4px]">*</p>
@@ -49,7 +98,8 @@ const MenuCard = (props) =>{
 
                         {/* Card Button Container */}
                         <div id='card-button-container' className="w-[70px] h-[50px] rounded-br-[20px] border-black border-[1px] border-dashed">
-                            {showViewButton()}
+                            {/* {showViewButton()} */}
+                            {showAddQuantity()}
                         </div>
 
                     </div>
