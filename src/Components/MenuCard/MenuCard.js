a68aaa32409d4fc32512df22addf65e769b33b9d
Bradley Parker <Bradleyscottparker@outlook.com>
2023-08-07 21:39:42 -0500

    created MenuImages directory in src, it contains menu item images

--- src/Components/MenuCard/MenuCard.js
+++ src/Components/MenuCard/MenuCard.js
@@ -1,5 +1,28 @@
 import minus from './minus.png'
 import plus from './plus.png'
+import breadSticks from '../../MenuImages/Breadsticks.png'
+import calamari from '../../MenuImages/Calamari.png'
+import chocolateCake from '../../MenuImages/chocolate cake.png'
+import chocolateChipCookie from '../../MenuImages/chocolate chip cookie.png'
+import cokeTwoLiter from '../../MenuImages/Coke 2 Liter.png'
+import cokeCan from '../../MenuImages/Coke Can.png'
+import deepDish from '../../MenuImages/deep dish.png'
+import dietCokeTwoLiter from '../../MenuImages/Diet Coke 2 Liter.png'
+import dietCokeCan from '../../MenuImages/Diet Coke Can.png'
+import fantaTwoLiter from '../../MenuImages/Fanta 2 liter.png'
+import fantaCan from '../../MenuImages/Fanta Can.png'
+import handTossed from '../../MenuImages/Hand Tossed.png'
+import hotWings from '../../MenuImages/Hotwings.png'
+import mozzarellaSticks from '../../MenuImages/Mozzarella Sticks.png'
+import spriteTwoLiter from '../../MenuImages/Sprite 2 Liter.png'
+import spriteCan from '../../MenuImages/Sprite Can.png'
+import thinCrust from '../../MenuImages/Thin crust.png'
+import vanillaIceCream from '../../MenuImages/Vanilla Ice Cream.png'
+
+
+
+
+
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -12,8 +35,9 @@ const MenuCard = (props) =>{
 
     const handleQuantityChange = (change) => {
 
-        if(change === 'decrease'  && quantity > 0){
+        if(change === 'decrease'  && quantity > 1){
             setQuantity(quantity-1)
+            
         }
         if(change === 'increase'  && quantity < 10){
             setQuantity(quantity+1)
@@ -21,9 +45,15 @@ const MenuCard = (props) =>{
         if(change === 'remove'){
             setQuantity(0)
         }
-        if(quantity === 0){
+        if(change === 'decrease' && quantity === 1){
             setInCart(false)
-            
+            setQuantity(0)
+
+
+        }
+        if(change === 'increase' && quantity === 0){
+            setInCart(true)
+            setQuantity(1)
         }
 
     }
@@ -44,20 +74,20 @@ const MenuCard = (props) =>{
 
         if(!inCart){
             return(
-            <div onClick={''} id='view-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
+            <div onClick={()=>{handleQuantityChange('increase')}} id='view-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
                 <p style={{fontSize:'14px', lineHeight:'14px'}} className="mt-[7px]  font-sergioTrendy text-white">Add</p>
             </div>                
             )
         }else{
             return(
-                <div id='quantity-container' className='flex h-[35px] w-[67px] border-black border-[1px] border-dashed'>
-                        <div id='minus-button'>
+                <div id='quantity-container' className='flex w-[70px] h-[50px] rounded-br-[20px] border-black border-[1px] border-dashed'>
+                        <div onClick={()=>{handleQuantityChange('decrease')}} id='minus-button' className='mt-[11px]'>
                             <img src={minus} className='h-[15px] w-[15px]'/>
                         </div>
-                        <div onClick={()=>{handleQuantityChange('decrease')}} id ='quantity-number'>
-                            {quantity}
+                        <div id ='quantity-number' className='ml-[7px] mr-[7px] flex justify-center border-black border-solid border-[1px] rounded-[3px] w-[25px] h-[35px]'>
+                            <p style={{fontSize:'16px', lineHeight:'16px'}} className='mt-[9px] font-sergioTrendy'>{quantity} </p>
                         </div>
-                        <div onClick={()=>{handleQuantityChange('increase')}} id='plus-button'>
+                        <div onClick={()=>{handleQuantityChange('increase')}} id='plus-button' className='mt-[11px]'>
                             <img src={plus} className='h-[15px] w-[15px]'/>
                         </div>
                 </div>
@@ -70,8 +100,8 @@ const MenuCard = (props) =>{
             <div id='menu-card-inner-container' className="h-full w-full bg-white rounded-[20px]">
                 
                 {/* Upper Card Area */}
-                <div id='upper-card-area' className="h-[140px] w-full rounded-tl-[20px] rounded-tr-[20px] border-black border-[1px] border-dashed">
-
+                <div id='upper-card-area' className="flex justify-center h-[140px] w-full rounded-tl-[20px] rounded-tr-[20px] border-black border-[1px] border-dashed">
+                    <img style={{height:'128px', width:'122px'}} src={deepDish}/>
                 </div>
 
                 {/* Lower Card Area */}
@@ -115,3 +145,20 @@ const MenuCard = (props) =>{
 }
 
 export default MenuCard;
+
+// const twoLiterSprite = {
+//     name: '2 Liter Sprite',
+//     image: 'Sprite 2 Liter.png',
+//     imageHeight:'121.67px',
+//     imageWidth:'50px',
+//     price: 2.50
+
+// }
+// const spriteCan = {
+//     name: 'Sprite Can',
+//     image: 'Sprite Can.png',
+//     imageHeight:'90px',
+//     imageWidth:'49.93px',
+//     price: 1
+
+// }
