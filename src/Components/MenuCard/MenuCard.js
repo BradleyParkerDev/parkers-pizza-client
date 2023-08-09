30b4bfe73157178e4560aecfb465bc1ac6c0c3c2
Bradley Parker <Bradleyscottparker@outlook.com>
2023-08-09 18:39:19 -0500

    mapped MenuCards to MenuPage

--- src/Components/MenuCard/MenuCard.js
+++ src/Components/MenuCard/MenuCard.js
@@ -29,20 +29,20 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const MenuCard = (props) =>{
     const navigate = useNavigate()
-    const [quantity, setQuantity] = useState(1);
-    const [inCart, setInCart] = useState(true)
+    const [quantity, setQuantity] = useState(0);
+    const [inCart, setInCart] = useState(false)
     const user = useSelector((state)=>state.users)
 
     const {
-        beverage
+        pizza,
+        beverage,
+        dessert,
+        side
     } = props
-    console.log(beverage.type)
-    // console.log(props)
-    const handleQuantityChange = (change) => {
 
+    const handleQuantityChange = (change) => {
         if(change === 'decrease'  && quantity > 1){
-            setQuantity(quantity-1)
-            
+            setQuantity(quantity-1)            
         }
         if(change === 'increase'  && quantity < 10){
             setQuantity(quantity+1)
@@ -53,48 +53,34 @@ const MenuCard = (props) =>{
         if(change === 'decrease' && quantity === 1){
             setInCart(false)
             setQuantity(0)
-
-
         }
-        if(change === 'increase' && quantity === 0){
+        if(change === 'add' && quantity === 0){
             setInCart(true)
             setQuantity(1)
         }
-
     }
     
 
 
 
-
-
-
-
-
-
-    const showViewButton = () =>{
-        
+    const showViewButton = () =>{   
         return(
             <div id='view-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
                 <p onClick={()=>{navigate('/pizza-builder')}} style={{fontSize:'14px', lineHeight:'14px'}} className="mt-[7px]  font-sergioTrendy text-white">View</p>
             </div>                
-        )
-  
-        
+        )  
 
     }
     const showAddQuantity = () =>{
-
-
         if(!inCart){
             return(
-            <div onClick={()=>{handleQuantityChange('increase')}} id='view-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
+            <div onClick={()=>{handleQuantityChange('add')}} id='add-button' className="mt-[8px] ml-[10px] flex justify-center w-[50px] h-[30px] rounded-[5px] bg-red-pp">
                 <p style={{fontSize:'14px', lineHeight:'14px'}} className="mt-[7px]  font-sergioTrendy text-white">Add</p>
             </div>                
             )
         }else{
             return(
-                <div id='quantity-container' className='flex w-[70px] h-[50px] rounded-br-[20px] border-black border-[1px] border-dashed'>
+                <div id='quantity-container' className='flex w-[70px] h-[50px] rounded-br-[20px]'>
                         <div onClick={()=>{handleQuantityChange('decrease')}} id='minus-button' className='mt-[11px]'>
                             <img src={minus} className='h-[15px] w-[15px]'/>
                         </div>
@@ -110,52 +96,59 @@ const MenuCard = (props) =>{
     }
     
     return(
-        <div id='menu-card-outer-container' className="w-[235px] h-[215px] p-[5px] bg-grey-pp rounded-[20px]">
+        <div id='menu-card-outer-container' className="w-[235px] h-[215px] p-[5px] mr-[30px] bg-grey-pp rounded-[20px]">
             <div id='menu-card-inner-container' className="h-full w-full bg-white rounded-[20px]">
                 
                 {/* Upper Card Area */}
-                <div id='upper-card-area' className="flex justify-center h-[140px] w-full rounded-tl-[20px] rounded-tr-[20px] border-black border-[1px] border-dashed">
-                    <img style={{height:`${beverage.imageHeight}`, width:`${beverage.imageWidth}`}} src={`${beverage.image}`}/>
+                <div id='upper-card-area' className="flex justify-center h-[140px] w-full rounded-tl-[20px] rounded-tr-[20px]">
+                    {pizza && <img style={{height:`${pizza.imageHeight}`, width:`${pizza.imageWidth}`,marginTop:`${pizza.imageMarginTop}`}} src={pizza.image}/>}                 
+                    {side && <img style={{height:`${side.imageHeight}`, width:`${side.imageWidth}`,marginTop:`${side.imageMarginTop}`}} src={side.image}/>}                 
+                    {dessert && <img style={{height:`${dessert.imageHeight}`, width:`${dessert.imageWidth}`,marginTop:`${dessert.imageMarginTop}`}} src={dessert.image}/>}                 
+                    {beverage && <img style={{height:`${beverage.imageHeight}`, width:`${beverage.imageWidth}`,marginTop:`${beverage.imageMarginTop}`}} src={beverage.image}/>}
+
                 </div>
 
                 {/* Lower Card Area */}
-                <div id='lower-card-area' className="h-[65px] w-full rounded-bl-[20px] rounded-br-[20px] border-black border-[1px] border-dashed">
-                    <div id='card-title' className="h-[15px] w-full flex justify-center border-black border-[1px] border-dashed">
-                        <p style={{fontSize:'14px', lineHeight:'14px'}}  className="font-sergioTrendy">{`${beverage.name}`}</p>
+                <div id='lower-card-area' className="h-[65px] w-full rounded-bl-[20px] rounded-br-[20px]">
+                    <div id='card-title' className="h-[15px] w-full flex justify-center">
+                        <p style={{fontSize:'14px', lineHeight:'14px'}}  className="font-sergioTrendy">
+                            {pizza && `${pizza.name}`}                           
+                            {side && `${side.name}`}                           
+                            {dessert && `${dessert.name}`}                           
+                            {beverage && `${beverage.name}`}
+                        </p>
                     </div>
                     <div id='card-price-button' className="flex ">
 
                         {/* Card Price Container */}
-                        <div id='card-price-container' className="w-[155px] h-[50px] rounded-bl-[20px] flex justify-end border-black border-[1px] border-dashed">
+                        <div id='card-price-container' className="w-[155px] h-[50px] rounded-bl-[20px] flex justify-end">
                             <div id='card-price' className="flex mt-[9px]">
                                 <div id='dollar-sign' >
                                     <p style={{fontSize:'24px', lineHeight:'24px'}} className="mr-[4px]">$</p>
                                 </div>
                                 <div id='price'>
-                                    <p style={{fontSize:'24px', lineHeight:'24px'}} className="font-sergioTrendy">{`${beverage.price}`}</p>
+                                    <p style={{fontSize:'24px', lineHeight:'24px'}} className="mr-[10px] font-sergioTrendy">
+                                        {pizza && `${pizza.price.toFixed(2)}`}                                        
+                                        {side && `${side.price.toFixed(2)}`}                                        
+                                        {dessert && `${dessert.price.toFixed(2)}`}
+                                        {beverage && `${beverage.price.toFixed(2)}`}
+                                    </p>
                                 </div>
-                                <div id='asterisk'>
-                                    <p style={{fontSize:'24px', lineHeight:'24px', fontFamily:'Arial'}} className="ml-[4px]">*</p>
-                                </div>
+                                {/* Pizza Asterisk */}
+                                {pizza && <div id='asterisk'> <p style={{fontSize:'24px', lineHeight:'24px', fontFamily:'Arial'}} className="ml-[0px]">*</p> </div>}
                             </div>
                         </div>
 
                         {/* Card Button Container */}
-                        <div id='card-button-container' className="w-[70px] h-[50px] rounded-br-[20px] border-black border-[1px] border-dashed">
-                            {/* {showViewButton()} */}
-                            {showAddQuantity()}
+                        <div id='card-button-container' className="w-[70px] h-[50px] rounded-br-[20px]">
+                            {pizza && showViewButton()}
+                            {!pizza && showAddQuantity()}
                         </div>
-
                     </div>
                 </div>
-
-
             </div>
         </div>
     )
-
-
-
 }
 
 export default MenuCard;
